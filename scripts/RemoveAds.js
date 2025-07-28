8933a5de96cb092b6403204e968e2d180f45f7f4
Maho Yoshino <62778359+Maho-Yoshino@users.noreply.github.com>
2025-07-28 21:31:28 +0200

    Improve ad and AI overview removal logic
    Updated RemoveAds.js to hide sponsored and shopping links instead of removing them, and enhanced AI overview removal to adjust padding for top visible elements. Also improved element selection to handle multiple matches and prevent redundant removals.

--- scripts/RemoveAds.js
+++ scripts/RemoveAds.js
@@ -1,34 +1,53 @@
 console.log('Remove "Sponsored" links extension loaded');
 ads_removed = false
+ai_removed = false
 
 function removeAds() {
-	const element = document.querySelector("div#tvcap");
+	const element = document.querySelectorAll("div#tvcap:not([style*='display: none'])");
 	if (element) {
-		element.remove();
-		console.log('removed sponsored links');
+		element.forEach((element) => {
+			element.style.display = 'none'
+		})
 	}
-	const cuContainer = document.querySelector("div.cu-container");
+	const cuContainer = document.querySelectorAll("div.cu-container:not([style*='display: none'])");
 	if (cuContainer) {
-		cuContainer.remove();
-		console.log('removed shopping section');
+		cuContainer.forEach((element) => {
+			element.style.display = 'none'
+		})
 	}
 	if (!(element||cuContainer)) {
+		console.log('removed sponsored and shopping links');
 		ads_removed = true
 	}
 }
 
 function removeAI() {
+	if (ai_removed) return
     chrome.storage.local.get("removeAI", (data) => {
-        if (!data.removeAI) return;
-        const element = document.querySelector("div.bzXtMb.M8OgIe.dRpWwb");
+        if (!data.removeAI) return
+        const element = document.querySelector("div.bzXtMb.M8OgIe.dRpWwb")
 		if (element) {
-        	element.remove()
+			const top_elements = document.getElementsByClassName("MjjYud")
+			if (top_elements) {
+				found_top_visible = false
+				Array.from(top_elements).forEach((element) => {
+					if (element.childList && !found_top_visible) {
+						element.style.paddingTop = "1.5em"
+						console.log("Set the padding for the top visible element to 1.5em")
+						found_top_visible = true
+					}
+				})
+			}
+
+        	element.style.display = 'none'
 			console.log("AI overview removed.")
+			ai_removed = true
+			return
 		}
     });
 }
 const observer = new MutationObserver(() => {
 	removeAI()
 	removeAds()
-});
-observer.observe(document.body, { childList: true, subtree: true });
+})
+observer.observe(document.body, { childList: true, subtree: true })
